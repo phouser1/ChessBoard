3cc878a7a213c9f01da8728e11d35ce9a4a163c4
ReventonDiablo <paul.houser@hotmail.com>
2020-10-12 04:31:27 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    DragDropModule,
     AppRoutingModule
   ],
   providers: [],
+  exports: [DragDropModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
